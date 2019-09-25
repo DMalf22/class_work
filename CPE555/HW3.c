b45b7b0950def60219052fbda4dc9f81fb5548ce
Dom Malfetano <dmalfetano@verizon.net>
2019-09-25 16:34:38 -0400

    HW3

---
+++ CPE555/HW3.c
@@ -0,0 +1,66 @@
+/*
+Domenic Malfetano
+CPE 555 HW 3
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int latD, latM, lonD, lonM; //units for output
+  float lon, lat, temp, latS, lonS; //input for latitude and longitude
+  char ns, we; //cardinal directions
+
+  //Input
+  printf("Input Latitude: ");
+  scanf("%f", &lat);
+  printf("Input Longitude: " );
+  scanf("%f", &lon);
+
+  if(lat < 0){
+    ns = 'S';
+    lat*= -1;
+  }
+
+  else if (lat > 0){
+    ns = 'N';
+  }
+
+  else {
+    ns = ' ';
+  }
+  if(lon < 0){
+    ew = 'W';
+    lon*= -1;
+  }
+
+  else if (lon > 0){
+    ew = 'E';
+  }
+
+  else {
+    ew = ' ';
+  }
+
+  latD = (int) lat;
+  lat = lat - latD;
+  temp = lat * 60;
+  latM = (int) temp;
+  lat = lat - latM;
+  temp = lat * 60;
+  latS = temp;
+
+  lonD = (int) lon;
+  lon = lon - lonD;
+  temp = lon * 60;
+  lonM = (int) temp;
+  lon = lon - lonM;
+  temp = lon * 60;
+  lonS = temp;
+
+  printf("Latitude: %d Degrees, %d Minutes, %f Seconds, %c ", latD, latM, latS, ns);
+  printf("Longitude: %d Degrees, %d Minutes, %f Seconds, %c ", lonD, lonM, lonS, ew);
+
+  return 0;
+
+}

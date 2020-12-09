3568f481f33d3ad4ecf718837533668b5c0cec1f
Dom Malfetano <55170800+DMalf22@users.noreply.github.com>
2020-12-09 18:03:23 -0500

    Update STAT.py

--- EE551/STAT.py
+++ EE551/STAT.py
@@ -1,7 +1,9 @@
+#open the webpage and parse for data using Selenium
 from selenium import webdriver
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get("https://www.baseball-reference.com/players/j/jeterde01.shtml")
 
+#create dictionary for stats
 player = {
 
     "WAR": {"xpath": '//*[@id="info"]/div[4]/div[2]/div[1]/p', "value" : "" },
@@ -17,12 +19,16 @@ player = {
     "OPS" : {"xpath": '//*[@id="info"]/div[4]/div[4]/div[3]/p', "value" : "" },
 }
 
+#assign value of stat from wepage using the xpath
 for stat in player.keys():
     player[stat]["value"] = driver.find_element_by_xpath(player[stat]["xpath"]).text
 
+#close driver and webpage 
 driver.close()
 
+#run loop for user to choose a stat
 while True:
+#stat interface 
     print("\nSelect Derek Jeter Career Stat:\n")
 
     for stat in player.keys():
@@ -32,8 +38,10 @@ while True:
     print('"R" = Runs Scored, "RBI" = Runs Batted In, "SB" = Stolen Bases "OBP" = On Base %, "SLG" = Slugging %, "OPS" = On Base + Slugging %')
     print('\n"Quit" to exit\n')
 
+#user inputs stat
     mychoice = input('Choice: ')
-
+    
+#cases for what is returned to user depending on their input 
     if mychoice in player.keys():
         print(mychoice,':',player[mychoice]["value"])
     elif mychoice == "Quit":
